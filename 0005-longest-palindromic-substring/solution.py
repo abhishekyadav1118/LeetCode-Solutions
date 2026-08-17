a53eb372d0bcf6f13a456f9ab15aa07737dfd63b
abhishek <abhishekyadav4203689@gmail.com>
2026-08-18 00:20:56 +0530

    Solve Longest Palindromic Substring (python3)

---
+++ 0005-longest-palindromic-substring/solution.py
@@ -0,0 +1,30 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if not s:
+            return ""
+
+        start, end = 0, 0
+
+        def expand_around_center(left: int, right: int) -> int:
+            # Expand outwards as long as characters match and indices are valid
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            # Return the length of the palindrome found
+            return right - left - 1
+
+        for i in range(len(s)):
+            # Case 1: Odd length palindrome (center is i)
+            len1 = expand_around_center(i, i)
+            # Case 2: Even length palindrome (center is between i and i+1)
+            len2 = expand_around_center(i, i + 1)
+
+            # Get the maximum length found at this center
+            max_len = max(len1, len2)
+
+            # If a longer palindrome is found, update the start and end indices
+            if max_len > end - start:
+                start = i - (max_len - 1) // 2
+                end = i + max_len // 2
+
+        return s[start : end + 1]
