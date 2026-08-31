887f172a4a6e9d6acf72d6317757bd205fe0bdbe
abhishek <abhishekyadav4203689@gmail.com>
2026-08-31 23:29:05 +0530

    Solve Plus One (python3)

---
+++ 0066-plus-one/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def plusOne(self, digits: list[int]) -> list[int]:
+        # Traverse the array backwards
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            # If the digit is 9, it becomes 0
+            digits[i] = 0
+
+        # If all digits were 9, we need an extra 1 at the front
+        return [1] + digits
